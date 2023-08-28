b7a2fc2fa7e49245f066c89c44d36c583948b8d6
Roman Osipov <ramsescrazy@mail.ru>
2023-08-28 22:28:50 +0300

    add final corrections

--- dbo.vw_SKUPrice.sql
+++ dbo.vw_SKUPrice.sql
@@ -3,6 +3,9 @@ DROP VIEW dbo.vw_SKUPrice
 GO
 CREATE VIEW dbo.vw_SKUPrice
 AS
-SELECT *, dbo.udf_GetSKUPrice(ID) AS CalculatedPrice
-FROM dbo.SKU;
+SELECT 
+*
+,dbo.udf_GetSKUPrice(ID) AS CalculatedPrice
+FROM 
+	dbo.SKU;
 GO
