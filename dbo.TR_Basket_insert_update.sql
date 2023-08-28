3eec359dc3b641b2322c10ccd019fbd5fbf70d92
Roman Osipov <ramsescrazy@mail.ru>
2023-08-28 22:43:36 +0300

    add final repairing

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,16 +1,33 @@
 IF OBJECT_ID('dbo.TR_Basket_insert_update') IS NOT NULL
 	DROP TRIGGER dbo.TR_Basket_insert_update
 GO
-CREATE TRIGGER dbo.TR_Basket_insert_update ON dbo.Basket 
-	AFTER INSERT, UPDATE
-		AS
-			BEGIN
-	UPDATE DiscountValue 
-	SET DiscountValue = CASE
-		WHEN (SELECT Quantity FROM INSERTED) >= 2 THEN (SELECT Value FROM INSERTED) * 0.05 
-    ELSE 0
-		END
-	WHERE ID_SKU = (SELECT ID_SKU FROM inserted);
-		END;
+CREATE TRIGGER 
+	dbo.TR_Basket_insert_update ON dbo.Basket 
+AFTER INSERT, UPDATE
+	AS
+		BEGIN
+UPDATE 
+	DiscountValue 
+SET 
+	DiscountValue = CASE
+WHEN (
+SELECT 
+	Quantity 
+FROM 
+	INSERTED) >= 2 
+THEN (
+SELECT 
+	Value 
+FROM 
+	INSERTED) * 0.05 
+ELSE 0
+END
+WHERE 
+	ID_SKU = (
+SELECT 
+	ID_SKU 
+FROM 
+	inserted);
+END;
 GO
 	
