eebb88b954745931fe4dbbef160423620e009fc2
Roman Osipov <ramsescrazy@mail.ru>
2023-08-28 22:35:41 +0300

    add final correction

--- dbo.TR_Basket_insert_update.sql
+++ dbo.TR_Basket_insert_update.sql
@@ -1,16 +1,16 @@
 IF OBJECT_ID('dbo.TR_Basket_insert_update') IS NOT NULL
-DROP TRIGGER dbo.TR_Basket_insert_update
+	DROP TRIGGER dbo.TR_Basket_insert_update
 GO
 CREATE TRIGGER dbo.TR_Basket_insert_update ON dbo.Basket 
 	AFTER INSERT, UPDATE
-	AS
-    BEGIN
+		AS
+			BEGIN
 	UPDATE DiscountValue 
 	SET DiscountValue = CASE
-	WHEN (SELECT Quantity FROM INSERTED) >=2 THEN (SELECT Value FROM INSERTED) * 0.05 
+		WHEN (SELECT Quantity FROM INSERTED) >= 2 THEN (SELECT Value FROM INSERTED) * 0.05 
     ELSE 0
-	END
+		END
 	WHERE ID_SKU = (SELECT ID_SKU FROM inserted);
-	END;
-	GO
+		END;
+GO
 	
